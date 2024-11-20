479b16e6484f347d39c6b4ebfe1e1373aa023bbc
Dao An Phuoc <anphuocdao2410@gmail.com>
2024-11-21 00:58:06 +0700

    Lottie JSON dark mode

--- src/components/displayLottie/DisplayLottie.js
+++ src/components/displayLottie/DisplayLottie.js
@@ -1,14 +1,36 @@
-import React, {Component, Suspense} from "react";
+import React, { Component, Suspense } from "react";
 import Lottie from "lottie-react";
 import Loading from "../../containers/loading/Loading";
 
 export default class DisplayLottie extends Component {
+  updateAnimationColors(animationData, isDarkMode) {
+    const updatedData = JSON.parse(JSON.stringify(animationData)); // Deep copy the animation data
+
+    updatedData.layers.forEach(layer => {
+      if (layer.shapes) {
+        layer.shapes.forEach(shape => {
+          if (shape.it) {
+            shape.it.forEach(item => {
+              if (item.ty === "fl" && item.c && item.c.darkMode) {
+                item.c.k = isDarkMode ? [0, 0, 0, 1] : [1, 0, 0, 1]; // Change color based on dark mode
+              }
+            });
+          }
+        });
+      }
+    });
+
+    return updatedData;
+  }
+
   render() {
-    const animationData = this.props.animationData;
+    const { animationData, isDarkMode } = this.props;
+    const updatedAnimationData = this.updateAnimationColors(animationData, isDarkMode);
+
     const defaultOptions = {
       loop: true,
       autoplay: true,
-      animationData: animationData
+      animationData: updatedAnimationData,
     };
 
     return (
--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -2,7 +2,7 @@ import React, {useContext} from "react";
 import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.scss";
-import landingPerson from "../../assets/lottie/landingPerson";
+import coding from "../../assets/lottie/coding";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
@@ -57,7 +57,7 @@ export default function Greeting() {
           </div>
           <div className="greeting-image-div">
             {illustration.animated ? (
-              <DisplayLottie animationData={landingPerson} />
+              <DisplayLottie animationData={coding} isDarkMode={isDark} />
             ) : (
               <img
                 alt="man sitting on table"
